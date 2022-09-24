36bc67abea1b16b5f456f81589f9ff523dbaf3c8
Poloni141 <nicolaslazaro.m@gmailcom>
2022-09-24 16:52:30 -0300

    Primera pre-entrega Nicolas Lazaro

---
+++ src/components/CartIcon/Carticon.jsx
@@ -0,0 +1,13 @@
+import '../CartIcon/Carticon.css'
+
+
+const CartIcon = () => {
+    return (
+        <div className='Cart'>
+            <img src='../../../public/cart.svg' alt='Cart Icon'/>
+            <p>0</p>
+        </div>
+    )
+}
+
+export default CartIcon
---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,9 @@
+const ItemListContainer = ({ greetings }) => {
+  return (
+    <div className="ItemListContainer">
+      <h4>{greetings}</h4>
+    </div>
+  );
+};
+
+export default ItemListContainer;
